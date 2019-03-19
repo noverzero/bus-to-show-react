72cade993d25f278539f38ee5e9a27655d61cf2c
Jake Mosher <jakemo136@gmail.com>
2019-03-19 09:46:52 -0600

    formatting

---
+++ src/Components/Admin/PickupsList.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import '../../App.css';
+
+
+const PickupsList = (props) => {
+  let { pickupLocations, makeSelection } = props
+  // console.log(pickupLocations)
+  return (
+    <div className='Pickups'>
+      {pickupLocations.map(location =>
+        <li className="list-group-item admin-list-item" 
+            key={location.id} 
+            id={location.id}>
+          <div className="row" id={location.id}>
+            <div className="col-md-10 list-item-font" id={location.id}> 
+              <strong style={{fontSize: '18px'}}>{location.locationName}</strong>
+              <br />
+              {location.streetAddress}
+              <br />
+            </div>
+              <button
+                id={location.id}
+                onClick={e => makeSelection('pickupLocationId', location.id, 'ReservationsList')}
+                type="button"
+                className='btn admin-detail-btn my-4 col-md-2'>
+                Select
+              </button>
+          </div>
+        </li>)}
+    </div>
+  )
+}
+
+export default PickupsList;
---
+++ src/Components/Admin/ReservationsList.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import '../../App.css';
+
+
+const ReservationsList = (props) => {
+  let { reservations, toggleCheckedIn } = props
+  
+  reservations = reservations.sort((a, b) => {
+    if(a.willCallLastName < b.willCallLastName) {return -1}
+    if(a.willCallLastName > b.willCallLastName) {return 1}
+    if(a.willCallLastName === b.willCallLastName) return (a.id - b.id)
+    return 0
+  })
+
+  return (
+    <div className='Reservations'>
+      {reservations.length > 0 ?
+        
+        reservations.map(reservation => {
+          const { willCallFirstName, willCallLastName, orderedByFirstName, orderedByLastName } = reservation
+          const lastName = willCallLastName
+          const firstName = willCallFirstName
+
+          if (reservation.status === '2') return null
+
+          return <li className="list-group-item admin-list-item" 
+            key={reservation.id} 
+            id={reservation.id}
+            style={{  borderRadius: '1px', padding: '.1rem .5rem' }}>
+          <div className="row" id={reservation.id}>
+            <div className="col-sm-8 list-item-font" id={reservation.id}> 
+              <strong style={{fontSize: '18px'}}>{lastName}, {firstName}</strong>
+              <br />
+              Alt: {orderedByLastName}, {orderedByFirstName}
+            </div>
+            <div className="col-sm-4" id={reservation.id}>
+              <label className="switch">
+                <input type="checkbox" 
+                className="default" 
+                checked={reservation.status === 1 ? 'checked' : ''}
+                onChange={event=>toggleCheckedIn(event.target.checked, reservation)} />
+                <span className="slider round"></span>
+              </label>
+            </div>
+          </div>
+        </li>
+        }
+        )
+        : 'Nobody to pick up!'}
+    </div>
+  )
+}
+
+export default ReservationsList;
