25b13ac5346bfbc17521a83a4d796ad46cff1d5f
dustinhuth <dustin@undefinedindustries.com>
2019-07-11 20:14:09 -0600

    re-sized headliner images and added contact info to home screen

--- src/Components/BannerRotator.js
+++ src/Components/BannerRotator.js
@@ -6,7 +6,26 @@ const BannerRotator = (props) => {
   return (
     <div className='banner px-2 mr-2 mb-4'>
       {props.displayShow ? '' :
-        <img src={banner} className="img-fluid" alt="bus to show red rocks planning is time travel" />}
+        <div>
+          <img src={banner} className="img-fluid" alt="bus to show red rocks planning is time travel" />
+          <div className='row mx-auto '>
+            <div className='col-6 border'>
+              <div className='row'>
+                <div className="conactText m-1">Reservation Support:
+                  <br/>reservations@bustoshow.org
+                </div>
+              </div>
+            </div>
+            <div className='col-6 border'>
+              <div className='row'>
+                <div className="conactText m-1">Private Events & Charters:
+                  <br/>parties@bustoshow.org
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      }
     </div>
 
 
