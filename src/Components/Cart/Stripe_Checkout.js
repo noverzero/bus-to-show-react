b14251d1618164f2b1fbfb3d0f8ddd18fae9544f
dustinhuth <dustin@undefinedindustries.com>
2019-10-19 22:01:43 -0600

    switched to test  keys for live testing

--- src/Components/Cart/Stripe_Checkout.js
+++ src/Components/Cart/Stripe_Checkout.js
@@ -2,8 +2,8 @@ import React from 'react';
 import StripeCheckout from 'react-stripe-checkout'
 
 //const fetchUrl = `http://localhost:3000`
-//const fetchUrl = `https://bts-test-backend.herokuapp.com`
-const fetchUrl = `https://innocuous-junior.herokuapp.com`
+const fetchUrl = `https://bts-test-backend.herokuapp.com`
+//const fetchUrl = `https://innocuous-junior.herokuapp.com`
 
 export default class Checkout extends React.Component {
 
