5c5cd3cc4a3efd8bb2b707b9fbb39048350ff7d7
Dustin <30206758+noverzero@users.noreply.github.com>
2023-03-26 01:47:54 -0600

    working on season passes

---
+++ src/Store.js
@@ -0,0 +1,22 @@
+import { create } from 'zustand'
+
+export const useStore = create((set) => ({
+  btsUser: {
+    isLoggedIn: false,
+    userID: '',
+    name: '',
+    email: '',
+    picture: '',
+    userDetails: {},
+  },
+  setBtsUser: (user) => set((state) => ({ btsUser: user })),
+
+  displayLoadingScreen: false,
+  setDisplayLoadingScreen: (bool) => set((state) => ({ displayLoadingScreen: bool })),
+
+  displayBus1: true,
+  setDisplayBus1: (bool) => set((state) => ({ displayBus1: bool })),
+
+  headerHidden: false,
+  setHideHeader: (bool) => set((state) => ({ headerHidden: bool })),
+}))
